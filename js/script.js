136b0574ac7a240f3cc1f82d6b1fa96e689b30d0
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2021-12-03 14:42:15 +0100

    push

---
+++ js/script.js
@@ -0,0 +1,53 @@
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            currentActiveSlide: 0,
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                }
+            ]
+        },
+        methods: {
+            showNextSlider: function() {
+                if (this.currentActiveSlide < this.slides.length - 1) {
+                    this.currentActiveSlide++;
+                } else{
+                    this.currentActiveSlide = 0;
+                }
+            },
+            showPreviousSlider: function(){
+                if (this.currentActiveSlide > 0) {
+                    this.currentActiveSlide--;
+                } else{
+                    this.currentActiveSlide = this.slides.length - 1;
+                }
+            }
+        }
+    }
+);
